04ed2f78662230df16528e320cc3e3bb020823ea
lilinze123 <96717375+lilinze123@users.noreply.github.com>
2025-09-14 21:20:00 +0800

    Add files via upload

---
+++ attackers/__init__.py
@@ -0,0 +1,24 @@
+
+
+from .prbcd import PRBCD
+from .greedy_rbcd import GreedyRBCD
+from .pga import PGA
+from .topology_attack import PGDAttack
+from .greedy import Greedy
+from .dice import DICE
+from .random_attack import RandomAttack
+from .sga import SGAttack
+from .gani import GANI
+
+
+attacker_map = {
+    'prbcd': PRBCD, 
+    'greedy-rbcd': GreedyRBCD,
+    'pga': PGA,
+    'pgdattack': PGDAttack,
+    'greedy': Greedy,
+    'dice': DICE,
+    'random': RandomAttack,
+    'sga': SGAttack,
+    'gani': GANI
+}
